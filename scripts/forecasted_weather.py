cce206b9634406783e3abf019ea84e9fb3a21793
popoykin <kukuruzische@gmail.com>
2026-05-05 20:48:54 +0400

    доработанный dag

--- scripts/forecasted_weather.py
+++ scripts/forecasted_weather.py
@@ -4,11 +4,8 @@ import psycopg2
 import time
 from psycopg2.extras import execute_batch
 from datetime import datetime
-from dotenv import load_dotenv
 import os
 import logging
-
-load_dotenv()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -104,7 +101,7 @@ def main():
                         "date",
                         max_temp,
                         min_temp,
-                        "precipitation forecast",
+                        precipitation_forecast,
                         date_receipt_of_forecast,
                         loaded_at
                     )
@@ -113,7 +110,7 @@ def main():
                     do update set
                         max_temp = excluded.max_temp,
                         min_temp = excluded.min_temp,
-                        "precipitation forecast" = excluded."precipitation forecast",
+                        precipitation_forecast = excluded.precipitation_forecast,
                         loaded_at = excluded.loaded_at;
                   """, batch_data)
 
@@ -127,64 +124,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-#import requests
-#import json
-#import psycopg2
-#from datetime import datetime
-#
-#DB_CONFIG = {
-#  "dbname": "postgres_practice_gpt",
-#  "user": "airflow",
-#  "password": "airflow",
-#  "host": "localhost",
-#  "port": "5432"
-#}
-#
-#API_URL = "https://api.open-meteo.com/v1/forecast"
-#
-#try:
-#  with psycopg2.connect(**DB_CONFIG) as conn:
-#    with conn.cursor() as cur:
-#
-#      cur.execute("""
-#        create schema if not exists raw;
-#
-#        create table if not exists raw.forecasted_weather
-#          (
-#            id serial primary key,
-#            city varchar(100),
-#            date date,
-#            max_temp float,
-#            min_temp float,
-#            "Precipitation Amount" float,
-#            "Precipitation date" date default current_date,
-#            loaded_at timestamp default now(),
-#            unique(city, date, "Precipitation date")
-#          )
-#      """)
-#
-#      cur.execute("select name, lat, lon from raw.city_coords;")
-#      rows = cur.fetchall()
-#      
-#      for name, lat, lon in rows:
-#        params = {
-#          "latitude": lat,
-#          "longitude": lon,
-#          "daily": "temperature_2m_max,temperature_2m_min,precipitation_sum",
-#          "timezone": "auto"
-#        }
-#        
-#        response = requests.get(API_URL, params=params)
-#        weather_data = response.json()
-#
-#        if "daily"  in weather_data:
-#          daily = weather_data["daily"]
-#
-#          for i in range(len(daily["time"])):
-#            cur.execute("""
-#              insert into raw.forecasted_weather(city, date, max_temp, min_temp, "Precipitation Amount") 
-#              values (%s, %s, %s, %s, %s)
-#              on conflict(city, date, "Precipitation date") do nothing;
-#            """,(name, daily["time"][i], daily["temperature_2m_max"][i], daily["temperature_2m_min"][i], daily["precipitation_sum"][i]))
-#except Exception as e:
-#  print(f"Ошибка {e}")
