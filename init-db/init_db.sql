d11e133d5a9aca89158a121c796e2f682c69260f
popoykin <kukuruzische@gmail.com>
2026-05-05 20:49:25 +0400

    доработанный dag

--- init-db/init_db.sql
+++ init-db/init_db.sql
@@ -1,1 +1,38 @@
 create database postgres_practice_gpt;
+
+create schema if not exists raw;
+
+create table if not exists raw.cities (
+	city text NULL,
+	country_code text NULL
+);
+
+CREATE TABLE if not exists raw.city_coords (
+	"name" varchar(100) NOT NULL,
+	lat float8 NULL,
+	lon float8 NULL,
+	country_code varchar(10) NOT NULL,
+	update_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT city_coords_pkey PRIMARY KEY (name, country_code)
+);
+
+CREATE TABLE if not exists raw.actual_weather (
+	city varchar(100) NULL,
+	"date" date NULL,
+	max_temp float8 NULL,
+	min_temp float8 NULL,
+	precipitation_forecast float8 NULL,
+	loaded_at timestamp DEFAULT now() NULL,
+	CONSTRAINT actual_weather_city_date_key UNIQUE (city, date)
+);
+
+CREATE TABLE if not exists raw.forecasted_weather (
+	city varchar(100) NOT NULL,
+	"date" date NOT NULL,
+	max_temp float8 NULL,
+	min_temp float8 NULL,
+	precipitation_forecast float8 NULL,
+	date_receipt_of_forecast date DEFAULT CURRENT_DATE NOT NULL,
+	loaded_at timestamp DEFAULT now() NULL,
+	CONSTRAINT forecasted_weather_pkey PRIMARY KEY (city, date, date_receipt_of_forecast)
+);
--- scripts/actual_weather.py
+++ scripts/actual_weather.py
@@ -1,10 +1,10 @@
 import requests
 import psycopg2
-from datetime import datetime, timedelta
-from dotenv import load_dotenv
+from psycopg2.extras import execute_batch
+from datetime import datetime, timedelta, timezone
+import time
+import logging
 import os
-
-load_dotenv()
 
 DB_CONFIG = {
   "dbname": os.getenv("DB_NAME"),
@@ -16,45 +16,107 @@ DB_CONFIG = {
 
 HISTORICAL_API_URL = "https://archive-api.open-meteo.com/v1/archive"
 
-end_date = datetime.now().strftime('%Y-%m-%d')
-start_date = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
+
+def get_cities(cur):
+    cur.execute("""
+        select name, lat, lon
+        from raw.city_coords;
+    """)
+    return cur.fetchall()
+
+def fetch_actual_weather(lat, lon, end_date, start_date, retries=3):
+    params={
+        "latitude":lat,
+        "longitude":lon,
+        "daily":"temperature_2m_max,temperature_2m_min,precipitation_sum",
+        "end_date": end_date,
+        "start_date": start_date
+        }
+
+    for attempt in range(retries):
+        try:
+            response = requests.get(HISTORICAL_API_URL, params=params, timeout=10)
+            response.raise_for_status()
+
+            data = response.json()
+
+            if "daily" not in data:
+                logging.warning(f"Нет данных блока для координат {lat}, {lon}")
+                return None
+
+            return data["daily"]
+
+        except Exception as e:
+            logging.error(f"{lat}, {lon} | попытка {attempt+1} | {e}")
+            time.sleep(2 ** attempt)
+
+    return None
+
 
 def main():
-  try:
-    with psycopg2.connect(**DB_CONFIG) as conn:
-      with conn.cursor() as cur:
+    logging.info(f"START load_actual_weather")
 
-        cur.execute("select name, lat, lon from raw.city_coords;")
-        rows = cur.fetchall()
-        
-        for name, lat, lon in rows:
-          params = {
-            "latitude": lat,
-            "longitude": lon,
-            "daily": "temperature_2m_max,temperature_2m_min,precipitation_sum",
-            "end_date": end_date,
-            "start_date": start_date
-          }
-          
-          response = requests.get(HISTORICAL_API_URL, params=params)
-          weather_data = response.json()
+    end_date = datetime.now().strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
 
-          if "daily"  in weather_data:
-            daily = weather_data["daily"]
+    try:
+        with psycopg2.connect(**DB_CONFIG) as conn:
+            with conn.cursor() as cur:
 
-            for i in range(len(daily["time"])):
-              cur.execute("""
-                insert into raw.actual_weather("city", "date", "max_temp", "min_temp", precipitation_forecast) 
-                values (%s, %s, %s, %s, %s)
-                on conflict("city", "date") 
-                do update set 
-                  "max_temp" = excluded."max_temp",
-                  "min_temp" = excluded."min_temp",
-                  precipitation_forecast = excluded.precipitation_forecast,
-                  loaded_at = now()
-              """,(name, daily["time"][i], daily["temperature_2m_max"][i], daily["temperature_2m_min"][i], daily["precipitation_sum"][i]))
-  except Exception as e:
-    print(f"Ошибка {e}")
+                cities = get_cities(cur)
+
+                if not cities:
+                    logging.warning(f"Нет горов для обработки")
+                    return
+
+                logging.info(f"Городов: {len(cities)}")
+
+                batch_data = []
+
+                for name, lat, lon in cities:
+                    logging.info(f"Обработка: {name}")
+
+                    daily = fetch_actual_weather(lat, lon, end_date, start_date)
+
+                    if daily is None:
+                        continue
+                    
+                    dates = daily["time"]
+                    max_temp = daily["temperature_2m_max"]
+                    min_temp = daily["temperature_2m_min"]
+                    precipitation_forecast = daily["precipitation_sum"]
+
+                    for i in range(len(dates)):
+                        batch_data.append((
+                            name,
+                            dates[i],
+                            max_temp[i],
+                            min_temp[i],
+                            precipitation_forecast[i]
+                        ))
+
+                    if not batch_data:
+                        raise Exception("Нет данных для загрузки actual_weather")
+                    
+                    execute_batch(cur, """
+                        insert into raw.actual_weather(city, date, max_temp, min_temp, precipitation_forecast) 
+                        values (%s, %s, %s, %s, %s)
+                        on conflict("city", "date") 
+                        do update set 
+                          "max_temp" = excluded."max_temp",
+                          "min_temp" = excluded."min_temp",
+                          precipitation_forecast = excluded.precipitation_forecast,
+                          loaded_at = now()
+                    """, batch_data)
+
+                    logging.info(f"Загруженно строк: {len(batch_data)}")
+
+    except Exception as e:
+        logging.error(f"CRITICAL: {e}")
+        raise
+    
+    logging.info("END load_forecast")
+
 
 if __name__ == "__main__":
     main()
